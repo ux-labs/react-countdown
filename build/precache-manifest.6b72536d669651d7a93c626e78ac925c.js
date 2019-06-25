df8c206ee7629f6af8f1c9eed7446a01d37bc1ba
Giovanni Mesquita <giovannimesquita@gmail.com>
2019-06-25 14:12:13 -0300

    Build do projeto, para execução offline.

---
+++ build/precache-manifest.6b72536d669651d7a93c626e78ac925c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc1086877696d5d29f0a5e46187069c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c9d5678db36997a494e",
+    "url": "/static/css/main.dd336919.chunk.css"
+  },
+  {
+    "revision": "aec9e88864570602a431",
+    "url": "/static/js/2.251bffa5.chunk.js"
+  },
+  {
+    "revision": "4c9d5678db36997a494e",
+    "url": "/static/js/main.461cacfc.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
